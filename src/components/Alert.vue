725b4b71432c3462babdbddc44daeb54fd66b3ef
Jose Luis Cabrera <cabrera@inkuba.com>
2022-12-02 17:25:47 -0300

    feat: alert components

---
+++ src/components/Alert.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+const props = defineProps<{
+    msg: string,
+    type: string
+}>()
+
+</script>
+
+<template>
+    <div class="m-4 alert shadow-lg w-2/3 mx-auto" :class="type">
+        <div v-if="msg" class="justify-center w-full">
+            {{ $t(msg) }}
+        </div>
+    </div>
+</template>
