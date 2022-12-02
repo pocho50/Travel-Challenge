6d2f17a46b9aaeadacc2859deb6eb92c29d8a1af
Jose Luis Cabrera <cabrera@inkuba.com>
2022-12-01 22:40:18 -0300

    feat: the navbar

---
+++ src/components/TheNavbar.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import { useDark, useToggle } from '@vueuse/core'
+import { SunIcon, MoonIcon } from '@heroicons/vue/24/outline/index'
+import AppButton from '@/components/AppButton.vue'
+
+const isDark = useDark({
+    attribute: 'data-theme',
+    valueDark: 'business',
+    valueLight: 'autumn'
+});
+const toggleDark = useToggle(isDark)
+</script>
+<template>
+
+    <div class="navbar bg-base-100 shadow-md">
+        <div class="container mx-auto">
+            <div class="flex-1">
+                <h1 class="text-xl">Travel challenge</h1>
+
+            </div>
+            <!-- Right Side -->
+            <div class="flex-none">
+                <AppButton :icon="true" @click="toggleDark()">
+                    <component :is="isDark ? MoonIcon : SunIcon" class="h-6 w-6" />
+                </AppButton>
+
+            </div>
+
+        </div>
+    </div>
+</template>
