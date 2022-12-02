442d500951ddcd8b45865d87112bd9ab24067d25
Jose Luis Cabrera <cabrera@inkuba.com>
2022-12-02 15:24:19 -0300

    feat: layovers component

---
+++ src/components/Layovers.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import type Segment from '@/types/Segment';
+import SegmentComponent from '@/components/Segment.vue'
+const props = defineProps<{
+    segments: Segment[]
+}>()
+
+</script>
+
+<template>
+    <div>
+        <div class="dropdown">
+            <label tabindex="0" class="btn btn-link m-1">{{ segments.length }} {{ $t('layovers') }}</label>
+            <div tabindex="0"
+                class="dropdown-content card card-compact w-96 p-2 shadow bg-neutral-content text-neutral-focus">
+                <div class="card-body">
+                    <SegmentComponent v-for="segment in segments" :key="segment.id" :segment="segment" :nonstop="false">
+                    </SegmentComponent>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,15 @@
 <script setup lang="ts">
 import SearchForm from '@/components/SearchForm.vue'
+import useOptions from '@/composables/useOptions';
+import Option from '@/components/Option.vue'
+
+const { fetchOptions, options } = useOptions()
+
+fetchOptions()
 
 </script>
 
 <template>
   <SearchForm />
+  <Option v-for="option in options" :key="option.id" :option="option"></Option>
 </template>
