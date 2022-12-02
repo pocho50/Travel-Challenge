f692455682a4db2fe9581bd4e503f5bb7268a6db
Jose Luis Cabrera <cabrera@inkuba.com>
2022-12-01 22:41:20 -0300

    feat: app button

---
+++ src/components/AppButton.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { computed } from '@vue/reactivity';
+const props = defineProps({
+    loading: { type: Boolean, default: false },
+    icon: { type: Boolean, default: false }
+});
+
+const btnclass = computed(() => {
+    return {
+        'btn-ghost btn-circle': props.icon,
+        loading: props.loading
+    }
+
+})
+</script>
+<template>
+    <button class="btn" :class="btnclass">
+        <slot></slot>
+    </button>
+</template>
